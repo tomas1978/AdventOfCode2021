632104b2abd9db3e29905c886ea445830215a41e
tomas1978 <tomas.nilsson78@gmail.com>
2021-12-03 19:18:22 +0100

    Day 1 gamma calculation hopefully correct

---
+++ day3/aoc2021_day3_1.py
@@ -0,0 +1,31 @@
+#answer:
+
+def convertBinArrayToInt(binArray):
+    decNumber=0
+    for i in range(len(binArray)):
+        if binArray[i]=='1':
+            decNumber+=2**(len(binArray)-1-i)
+    return decNumber
+
+f = open('input.txt','r')
+diagnostic = f.readlines()
+zeros=0
+ones=0
+gamma=['0','0','0','0','0','0','0','0','0','0','0','0']
+epsilon=['0','0','0','0','0','0','0','0','0','0','0','0']
+
+for position in range(len(diagnostic[0])-1):
+    for row in range(len(diagnostic)-1):
+        if diagnostic[row][position]=='0':
+            zeros+=1
+        else:
+            ones+=1
+    if ones>zeros:
+        gamma[position]='1'
+    else:
+        gamma[position]='0'
+    zeros=0
+    ones=0
+
+gammaDecimal=convertBinArrayToInt(gamma)
+print(gammaDecimal)
