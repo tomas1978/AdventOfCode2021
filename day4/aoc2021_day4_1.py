c8d525cfb5b21ffcadf8349518f8bc7464aa893e
tomas1978 <tomas.nilsson78@gmail.com>
2021-12-07 22:07:29 +0100

    Continued on the checkBingo function
    Still not finished

--- day4/aoc2021_day4_1.py
+++ day4/aoc2021_day4_1.py
@@ -3,13 +3,14 @@
 def checkBingo(board):
     bingoCounter=0
     for row in range(len(board)):
+        #bingoCounter=0
         for pos in range(len(board[row])):
             if board[row][pos]=='X':
                 bingoCounter+=1
-    if bingoCounter==3:
-        return True
-    else:
-        return False
+            if bingoCounter==3:
+                return True
+            else:
+                return False
 
 
 
@@ -37,10 +38,10 @@ while continueRead:
 
 
 print(boards[0])
-testBoard=[ [1, 2, 3],
-            [6, 4, 9],
-            [3, 7, 1],
-            [4, 7, 5] ]
+testBoard=[ ['X', 'X', 'X'],
+            ['X', 'X', 'X'],
+            ['X', 'X', 'X'],
+            ['4', '7', '5'] ]
 
 isBingo=checkBingo(testBoard)
 print(isBingo)
